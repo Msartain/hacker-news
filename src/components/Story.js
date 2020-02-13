1a3c6e2c07aff720052fe4297d55d0d18365e9e9
Matthew Sartain <matthewsartain@me.com>
2020-02-13 15:16:16 -0600

    adds styling to components using styled components

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,20 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import { getStory } from '../services/hnAPI'
+import React, { useState, useEffect } from "react";
+import { getStory } from "../services/hnAPI";
+import {
+  StoryWrapper,
+  StoryTitle,
+  StoryMeta,
+  StoryMetaElement
+} from "../styles/storyStyles";
 
 export const Story = ({ storyId }) => {
-    const [story, setStory] = useState({});
+  const [story, setStory] = useState({});
 
-    useEffect(() => {
-        //use the getStory func to get the story with the id, then if the 
-        //if the data that is returned has a url property, setState with the data
-        getStory(storyId).then(data => data && data.url && setStory(data))
-    }, []);
+  useEffect(() => {
+    //use the getStory func to get the story with the id, then if the
+    //if the data that is returned has a url property, setState with the data
+    getStory(storyId).then(data => data && data.url && setStory(data));
+  }, []);
 
-    return story && story.url ? (
-        <>
-        <a href={story.url}><p>{story.title}</p></a>
-        By: <p>{story.by}</p>
-        Posted: <p>{story.time}</p>
-        </>
-    ) : null;
-}
+  return story && story.url ? (
+    <StoryWrapper data-testid="story">
+
+      <StoryTitle>
+        <a href={story.url}>{story.title}</a>
+      </StoryTitle>
+
+      <StoryMeta>
+        <span className="story__by" data-testid="story-by">
+          <StoryMetaElement color="#000">By:</StoryMetaElement>
+          {story.by}
+        </span>
+        <span className="story__time" data-testid="story-time">
+          <StoryMetaElement color="#000">Posted:</StoryMetaElement>
+            {' '}{story.time}
+        </span>
+      </StoryMeta>
+
+    </StoryWrapper>
+  ) : null;
+};
--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,15 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import { getStoryIds } from '../services/hnAPI';
-import { Story } from '../components/Story'
+import React, { useEffect, useState } from "react";
+import { getStoryIds } from "../services/hnAPI";
+import { Story } from "../components/Story";
+import {
+  GlobalStyle,
+  StoriesContainerWrapper
+} from "../styles/StoriesContainerStyles";
 
 export const StoriesContainer = () => {
-    const [storyIds, setStoryIds] = useState([]);
+  const [storyIds, setStoryIds] = useState([]);
 
-    useEffect(() => {
-        getStoryIds().then(data => setStoryIds(data))
-    }, [])
+  useEffect(() => {
+    getStoryIds().then(data => setStoryIds(data));
+  }, []);
 
-    return storyIds.map(storyId => (
-        <Story key={storyId} storyId={storyId} />
-    ));
-}
+  return (
+    <>
+      <GlobalStyle />
+      <StoriesContainerWrapper data-test-id="stories-container">
+        <h1>Hacker News Stories</h1>
+        {storyIds.map(storyId => (
+          <Story key={storyId} storyId={storyId} />
+        ))}
+      </StoriesContainerWrapper>
+    </>
+  );
+};
